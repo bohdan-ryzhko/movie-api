77a2f8d0e2e06bd9a88b60ba41d22bcd581d7669
bohdan-ryzhko <m3meizumini@gmail.com>
2025-05-18 14:29:14 +0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,11 @@ app.use(setDefaultLanguage);
 app.use(i18n.init);
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  })
+);
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
